98c607c752718853c1a30330f73dfaaf7c36a9c6
Namanbukday <nbukday@gamil.com>
2019-10-23 18:48:23 +0530

    pygal/histogram

---
+++ Ch15_die_visual.py
@@ -0,0 +1,29 @@
+import pygal
+
+from Ch15_Die import Die
+
+# Create a D6.
+die = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for i in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Analyze the results (count each number):
+frequencies = []
+for i in range(1, die.num_sides+1):
+    freq = results.count(i)
+    frequencies.append(freq)
+
+# Visualize the results
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of the result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
